d100c4f6f22202be412888d327609ee85089157b
Mario Kerndler <e51822845@student.tuwien.ac.at>
2022-04-22 00:04:31 +0200

    Added the ability to descend into new depths

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,7 @@
 use rltk::{VirtualKeyCode, Rltk, Point};
 use specs::prelude::*;
 use std::cmp::{max, min};
+use crate::TileType;
 use super::{Position, Player, Viewshed, State, Map, RunState, CombatStats, WantsToMelee, Item, gamelog::GameLog, WantsToPickupItem};
 
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
@@ -36,6 +37,19 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
             ppos.x = pos.x;
             ppos.y = pos.y;
         }
+    }
+}
+
+pub fn try_next_level(ecs: &mut World) -> bool {
+    let player_pos = ecs.fetch::<Point>();
+    let map = ecs.fetch::<Map>();
+    let player_idx = map.xy_idx(player_pos.x, player_pos.y);
+    if map.tiles[player_idx] == TileType::DownStairs {
+        true
+    } else {
+        let mut gamelog = ecs.fetch_mut::<GameLog>();
+        gamelog.entries.push("There is no way down from here.".to_string());
+        false
     }
 }
 
@@ -104,6 +118,13 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) -> RunState {
 
             // Save and Quit
             VirtualKeyCode::Escape => return RunState::SaveGame,
+
+            // Level changes
+            VirtualKeyCode::Period => {
+                if try_next_level(&mut gs.ecs) {
+                    return RunState::NextLevel;
+                }
+            }
 
             _ => { return RunState::AwaitingInput  }
         },
