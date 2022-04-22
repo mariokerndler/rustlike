38a55c6845b67572a73a8fe5d6f117caedcf7a13
Mario Kerndler <e51822845@student.tuwien.ac.at>
2022-04-22 21:12:10 +0200

    Added bloodstains to the map.

--- src/map.rs
+++ src/map.rs
@@ -3,6 +3,7 @@ use super::{Rect};
 use std::cmp::{max, min};
 use specs::prelude::*;
 use serde::{Serialize, Deserialize};
+use std::collections::HashSet;
 
 pub const MAPWIDTH : usize = 80;
 pub const MAPHEIGHT : usize = 43;
@@ -23,6 +24,7 @@ pub struct Map {
     pub visible_tiles : Vec<bool>,
     pub blocked : Vec<bool>,
     pub depth : i32,
+    pub bloodstains: HashSet<usize>,
 
     #[serde(skip_serializing)]
     #[serde(skip_deserializing)]
@@ -94,7 +96,8 @@ impl Map {
             visible_tiles : vec![false; MAPCOUNT],
             blocked : vec![false; MAPCOUNT],
             tile_content : vec![Vec::new(); MAPCOUNT],
-            depth: new_depth
+            depth: new_depth,
+            bloodstains: HashSet::new()
         };
 
         const MAX_ROOMS : i32 = 30;
@@ -227,6 +230,7 @@ pub fn draw_map(ecs: &World, ctx : &mut Rltk) {
         if map.revealed_tiles[idx] {
             let glyph;
             let mut fg;
+            let mut bg = RGB::from_f32(0., 0., 0.);
             match tile {
                 TileType::Floor => {
                     glyph = rltk::to_cp437('.');
@@ -241,8 +245,12 @@ pub fn draw_map(ecs: &World, ctx : &mut Rltk) {
                     fg = RGB::from_f32(0., 1.0, 1.0);
                 }
             }
-            if !map.visible_tiles[idx] { fg = fg.to_greyscale() }
-            ctx.set(x, y, fg, RGB::from_f32(0., 0., 0.), glyph);
+            if map.bloodstains.contains(&idx) { bg = RGB::from_f32(0.75, 0., 0.); }
+            if !map.visible_tiles[idx] {
+                fg = fg.to_greyscale();
+                bg = RGB::from_f32(0., 0., 0.); // Don't show stains out of visual range
+            }
+            ctx.set(x, y, fg, bg, glyph);
         }
 
         // Move the coordinates
